1332b7f3b5a74a8ba38d90b2407b830ad5df3673
d.tarasov <mrsndmn46@gmail.com>
2025-10-11 20:21:35 +0300

    simple kv cache generation

--- handbook/manim/kv_cache/kv_cache.py
+++ handbook/manim/kv_cache/kv_cache.py
@@ -43,21 +43,21 @@ class KVCache(InteractiveScene):
         prefix_words = 2
         display_characters = sum(len(word) for word in text.split(" ")[:prefix_words])
 
-        self.play(Write(text_mob[:display_characters], stroke_color=BLUE_B))
-        self.wait()
-
         # Create word rects
-
-        # TODO add animation update func? Or explicitly draw boxes on next tokens generation.
         processed_letters = 0
         words = text.split(" ")
         all_rects = []
-        for word in words:
+        words_mobs = []
+        for word_i, word in enumerate(words):
             word_len = len(word)
             word_mob = text_mob[processed_letters:processed_letters + word_len]
-            rect = SurroundingRectangle(word_mob)
+            words_mobs.append(word_mob)
             processed_letters += word_len
 
+            if word_i >= prefix_words:
+                continue
+
+            rect = SurroundingRectangle(word_mob)
             rect.set_height(text_mob.get_height() + SMALL_BUFF, stretch=True)
             rect.set_width(word_mob.get_width() + (SMALL_BUFF / 5), stretch=True)
             rect.match_y(text_mob)
@@ -66,45 +66,31 @@ class KVCache(InteractiveScene):
             all_rects.append(rect)
 
         self.add(VGroup(*all_rects[:prefix_words]))
+        self.play(Write(text_mob[:display_characters], stroke_color=BLUE_B))
         self.wait()
 
-        # Adjectives updating noun
-        adjs = ["fluffy", "blue", "verdant"]
-        nouns = ["creature", "forest"]
-        others = ["a", "roamed", "the"]
-        adj_mobs, noun_mobs, other_mobs = [
-            VGroup(word2mob[substr] for substr in group)
-            for group in [adjs, nouns, others]
-        ]
-        adj_rects, noun_rects, other_rects = [
-            VGroup(word2rect[substr] for substr in group)
-            for group in [adjs, nouns, others]
-        ]
-        adj_rects.set_submobject_colors_by_gradient(BLUE_C, BLUE_D, GREEN)
-        noun_rects.set_color(GREY_BROWN).set_stroke(width=3)
-        kw = dict()
-        adj_arrows = VGroup(
-            Arrow(
-                adj_mobs[i].get_top(), noun_mobs[j].get_top(),
-                path_arc=-150 * DEGREES, buff=0.1, stroke_color=GREY_B
+        for word_i in range(prefix_words, len(words_mobs)):
+            word_mob = words_mobs[word_i]
+            rect = SurroundingRectangle(word_mob)
+            rect.set_height(word_mob.get_height() + SMALL_BUFF, stretch=True)
+            rect.set_width(word_mob.get_width() + (SMALL_BUFF / 2), stretch=True)
+            rect.match_y(word_mob)
+            rect.set_stroke(GREY, 1)
+            rect.set_fill(GREY, 0.25)
+
+            adj_arrows = VGroup(
+                Arrow(
+                    words_mobs[i].get_top(), word_mob.get_top(),
+                    path_arc=-150 * DEGREES, buff=0.1, stroke_color=GREY_B,
+                    thickness=1.0
+                )
+                for i in range(word_i)
             )
-            for i, j in [(0, 0), (1, 0), (2, 1)]
-        )
 
-        self.play(
-            LaggedStartMap(DrawBorderThenFill, adj_rects),
-            Animation(adj_mobs),
-        )
-        self.wait()
-        self.play(
-            LaggedStartMap(DrawBorderThenFill, noun_rects),
-            Animation(noun_mobs),
-            LaggedStartMap(ShowCreation, adj_arrows, lag_ratio=0.2, run_time=1.5),
-        )
+            self.add(rect, adj_arrows)
+            self.play(Write(word_mob, stroke_color=BLUE_B))
+            self.wait()
 
-        kw = dict(time_width=2, max_stroke_width=10, lag_ratio=0.2, path_arc=150 * DEGREES)
-        self.play(
-            ContextAnimation(noun_mobs[0], adj_mobs[:2], strengths=[1, 1], **kw),
-            ContextAnimation(noun_mobs[1], adj_mobs[2:], strengths=[1], **kw),
-        )
+            adj_arrows.clear()
+
         self.wait()
